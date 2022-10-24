81a9c2466b37be1a719f8cd428529a2a8f892a80
wisdomstar94 <wisdomstar94@gmail.com>
2022-10-24 21:53:36 +0900

    CharacterControlsAmmo 클래스 제작.

--- src/pages/threejs-example/threejs-008-character-moving-with-ammo/index.tsx
+++ src/pages/threejs-example/threejs-008-character-moving-with-ammo/index.tsx
@@ -3,231 +3,11 @@ import { useEffect, useRef } from "react";
 import CommonLayout from "../../../components/layouts/common-layout/common-layout.component";
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import * as CANNON from 'cannon-es';
 import { IThreejsCanvasBox } from "../../../components/boxes/threejs-canvas-box/threejs-canvas-box.interface";
 import ThreejsCanvasBox from "../../../components/boxes/threejs-canvas-box/threejs-canvas-box.component";
-import { ThreeAmmoObjectManager, ThreeCannonObject } from "../../../librarys/three-object-util/three-object-util.library";
+import { CharacterControlsAmmo, ThreeAmmoObjectManager, ThreeCannonObject } from "../../../librarys/three-object-util/three-object-util.library";
 import useFromEvent from "../../../hooks/use-from-event/use-from-event";
 import { GLTF, GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { IThreeObjectLibrary } from "../../../librarys/three-object-util/three-object-util.interface";
-
-const ControlKeys = {
-  ArrowTop: 'w',
-  ArrowLeft: 'a',
-  ArrowRight: 'd',
-  ArrowDown: 's',
-};
-
-class CharacterControls {
-  model: IThreeObjectLibrary.ThreeAmmoObjectItem;
-  mixer: THREE.AnimationMixer;
-  animationsMap: Map<string, THREE.AnimationAction> = new Map();
-  orbitControls: OrbitControls;
-  camera: THREE.PerspectiveCamera;
-
-  toggleRun: boolean = false;
-  toggleJump: boolean = false;
-  currentAction: string;
-
-  walkDirection = new THREE.Vector3();
-  rotateAngle = new THREE.Vector3(0, 1, 0);
-  // rotateAngle = new Ammo.btVector3(0, 1, 0);
-  rotateQuarternion: THREE.Quaternion = new THREE.Quaternion();
-  // rotateQuarternion = new Ammo.btQuaternion(0, 0, 0, 1);
-  cameraTarget = new THREE.Vector3();
-
-  fadeDuration: number = 0.2;
-  runVelocity = 5;
-  walkVelocity = 2;
-
-  // DIRECTIONS = ['w', 'a', 's', 'd'];
-
-  constructor(params: {
-    model: IThreeObjectLibrary.ThreeAmmoObjectItem,
-    mixer: THREE.AnimationMixer,
-    animationsMap: Map<string, THREE.AnimationAction>,
-    orbitControls: OrbitControls,
-    camera: THREE.PerspectiveCamera,
-    currentAction: string,
-  }) {
-    const {
-      model,
-      mixer,
-      orbitControls,
-      camera,
-      currentAction,
-      animationsMap,
-    } = params;
-
-    this.model = model;
-    this.mixer = mixer;
-    this.orbitControls = orbitControls;
-    this.camera = camera;
-    this.currentAction = currentAction;
-    this.animationsMap = animationsMap;
-    this.animationsMap.forEach((value, key) => {
-      if (key === currentAction) {
-        value.play();
-      }
-    });
-  }
-
-  switchRunToggle(value?: boolean) {
-    if (typeof value === 'boolean') {
-      this.toggleRun = value;
-      return;  
-    }
-
-    this.toggleRun = !this.toggleRun;
-  }
-
-  switchJumpToggle(value?: boolean) {
-    if (typeof value === 'boolean') {
-      this.toggleJump = value;
-      return;  
-    }
-
-    this.toggleJump = !this.toggleJump;
-  }
-
-  update(delta: number, keyPressed: any) {
-    // const directionPressed = Object.values(ControlKeys).some(key => keyPressed[key] === true);
-    const directionPressed = keyPressed[ControlKeys.ArrowDown] || keyPressed[ControlKeys.ArrowLeft] || keyPressed[ControlKeys.ArrowRight] || keyPressed[ControlKeys.ArrowTop];
-
-    let play = ''
-    if (this.toggleJump) {
-      play = 'Tpose';
-    } else if (directionPressed && this.toggleRun) {
-      play = 'Run';
-    } else if (directionPressed) {
-      play = 'Walk';
-    } else {
-      play = 'Idle';
-    }
-
-    if (this.currentAction !== play) {
-      const toPlay = this.animationsMap.get(play);
-      const current = this.animationsMap.get(this.currentAction);
-
-      current?.fadeOut(this.fadeDuration);
-      toPlay?.reset().fadeIn(this.fadeDuration).play();
-
-      this.currentAction = play;
-    }
-
-    this.mixer.update(delta);
-
-    if (directionPressed) {
-      const ammoTransform = this.model.getAmmoTransfrom(delta, this.model);
-      const ammoOrigin = ammoTransform.getOrigin();
-      const ammoRotation = ammoTransform.getRotation();
-
-      // calculate towards camera direction
-      const angleYCameraDirection = Math.atan2(
-        (this.camera.position.x - ammoOrigin.x()), 
-        (this.camera.position.z - ammoOrigin.z()),
-      );
-      // diagonal movement angle offset
-      const directionOffset = this.directionOffset(keyPressed);
-
-      // rotate model 
-      this.rotateQuarternion.setFromAxisAngle(this.rotateAngle, angleYCameraDirection + directionOffset);
-      // ammoRotation.setRotation(this.rotateAngle, angleYCameraDirection + directionOffset);
-      // this.model.ammoJsObject.setAngularVelocity(this.rotateQuarternion.getAxis());
-      // this.rotateQuarternion.setFromAxisAngle(this.rotateAngle, angleYCameraDirection + directionOffset);
-      this.model.threeJsObject.quaternion.rotateTowards(this.rotateQuarternion, 0.2);
-
-      // calculate direction
-      this.camera.getWorldDirection(this.walkDirection);
-      this.walkDirection.y = 0;
-      this.walkDirection.normalize();
-      this.walkDirection.applyAxisAngle(this.rotateAngle, directionOffset);
-
-      // run/walk velocity
-      const velocity = this.toggleRun ? this.runVelocity : this.walkVelocity;
-
-      // move model & camera
-      const prevPosition = { 
-        x: ammoOrigin.x(),
-        y: ammoOrigin.y(),
-        z: ammoOrigin.z(),
-      };
-      const moveX = this.walkDirection.x * velocity * delta;
-      const moveZ = this.walkDirection.z * velocity * delta;
-      
-      // console.log('prevPosition', prevPosition);
-      const nextPosition = {
-        x: ammoOrigin.x() + moveX,
-        z: ammoOrigin.z() + moveZ,
-      };
-      // console.log('nextPosition', nextPosition);
-      // ammoOrigin.setX(nextPosition.x);
-      // ammoOrigin.setZ(nextPosition.z);
-      // ammoOrigin.setY(5);
-      // console.log('moveX', moveX);
-      // console.log('moveZ', moveZ);
-
-      const velocity2 = this.model.ammoJsObject.getLinearVelocity();
-      let resultantImpulse = velocity2;
-      // resultantImpulse.op_mul(1);
-      // this.model.ammoJsObject.applyCentralImpulse(resultantImpulse);
-      velocity2.setX(velocity2.x() + moveX);
-      velocity2.setZ(velocity2.z() + moveZ);
-      console.log('setLinearVelocity');
-      this.model.ammoJsObject.setLinearVelocity(resultantImpulse);
-      // setTimeout(() => {
-      //   let _moveX = ammoOrigin.x() - prevPosition.x;
-      //   let _moveZ = ammoOrigin.z() - prevPosition.z;
-      //   console.log('currentPosition', { x: ammoOrigin.x(), y: ammoOrigin.y(), z: ammoOrigin.z(), });
-
-      //   this.updateCameraTarget(_moveX, _moveZ);
-      // });
-    } else if (this.toggleJump) {
-      this.cameraTarget.x = this.model.threeJsObject.position.x;
-      this.cameraTarget.y = this.model.threeJsObject.position.y;
-      this.cameraTarget.z = this.model.threeJsObject.position.z;
-      this.orbitControls.target = this.cameraTarget;
-    }
-  }
-
-  private updateCameraTarget(moveX: number, moveZ: number) {
-    // move camera
-    this.camera.position.x += moveX;
-    this.camera.position.z += moveZ;
-
-    // update camera target
-    this.cameraTarget.x = this.model.threeJsObject.position.x;
-    this.cameraTarget.y = this.model.threeJsObject.position.y;
-    this.cameraTarget.z = this.model.threeJsObject.position.z;
-    this.orbitControls.target = this.cameraTarget;
-  }
-
-  private directionOffset(keysPressed: any) {
-    let directionOffset = 0 // w
-
-    if (keysPressed[ControlKeys.ArrowTop]) {
-      if (keysPressed[ControlKeys.ArrowLeft]) {
-        directionOffset = Math.PI / 4 // w+a
-      } else if (keysPressed[ControlKeys.ArrowRight]) {
-        directionOffset = - Math.PI / 4 // w+d
-      }
-    } else if (keysPressed[ControlKeys.ArrowDown]) {
-      if (keysPressed[ControlKeys.ArrowLeft]) {
-        directionOffset = Math.PI / 4 + Math.PI / 2 // s+a
-      } else if (keysPressed[ControlKeys.ArrowRight]) {
-        directionOffset = -Math.PI / 4 - Math.PI / 2 // s+d
-      } else {
-        directionOffset = Math.PI // s
-      }
-    } else if (keysPressed[ControlKeys.ArrowLeft]) {
-      directionOffset = Math.PI / 2 // a
-    } else if (keysPressed[ControlKeys.ArrowRight]) {
-      directionOffset = - Math.PI / 2 // d
-    }
-
-    return directionOffset
-  }
-}
 
 const IndexPage = () => {
   return (
@@ -255,7 +35,7 @@ const PageContents = () => {
   const boxThreeCannonObjectsRef = useRef<Set<ThreeCannonObject>>(new Set<ThreeCannonObject>());
   const allObjectsRef = useRef<Set<THREE.Object3D<any>>>(new Set<THREE.Object3D<any>>());
 
-  const characterControlsRef = useRef<CharacterControls>();
+  const characterControlsRef = useRef<CharacterControlsAmmo>();
   const keyPressedRef = useRef<{ [key: string]: boolean }>({});
 
   useEffect(() => {
@@ -503,7 +283,7 @@ const PageContents = () => {
       animationsMap.set(a.name, mixer.clipAction(a));
     });
 
-    characterControlsRef.current = new CharacterControls({
+    characterControlsRef.current = new CharacterControlsAmmo({
       model: characterObject,
       mixer,
       animationsMap,
